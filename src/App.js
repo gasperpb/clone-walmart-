f34e90e8010eb05ad8651e75f1bf6c2257cc97a5
gasperpb <achillespb@hotmail.com>
2022-11-27 14:31:07 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* Navbar */}
+      <Navbar />
+      {/* Hero */}
+      {/* Deals */}
+      {/*For You  */}
+      {/* Footer */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <div>
+            {/* Left */}
+            <div className="">
+
+            </div>
+            {/* Middle */}
+            {/* Right */}
+        </div>
+    );
+};
